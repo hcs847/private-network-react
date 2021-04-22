d2b2700d83a28840b6c6d35ea27a0d3f42ed465d
Hila Caspi <caspi.home@gmail.com>
2021-04-21 18:31:43 -0700

    home page styling side containers

--- client/src/components/GroupList/index.js
+++ client/src/components/GroupList/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { RiGroup2Fill } from "react-icons/ri";
 
 const GroupList = ({ groups }) => {
     return (
         <>
-            <h3>
-                Groups
-        </h3>
+            <div className="flex flex-left groups">
+                <RiGroup2Fill className='icons title' />
+                <h3 className='title'>
+                    Groups
+                </h3>
+            </div>
             <ul className='group-list card'>
                 {groups &&
                     groups.map(group => (
